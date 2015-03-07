fc5767e80decd3556105c0e8de648d2e4274f696
githubforzhanglei <zhanglei19910607@163.com>
2015-03-07 14:30:00 +0800

    test

---
+++ Test/Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by Zhang.Lei on 15-3-7.
+//  Copyright (c) 2015年 zl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Test/Test/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Test
+//
+//  Created by Zhang.Lei on 15-3-7.
+//  Copyright (c) 2015年 zl. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
